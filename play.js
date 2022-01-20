9edafbb202c1cd58c6389d90bee703f1dc9562f7
Sarah Melanie White <sawrrawr@gmail.com>
2022-01-19 21:15:23 -0330

    moved the setupInput function and handleUserInput function to a file called input.js

--- play.js
+++ play.js
@@ -1,19 +1,8 @@
 const net = require('net');
 const connect = require('./client.js');
+const setupInput = require('./input');
 
 console.log('Connecting...');
 connect();
 
-
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput = (key) => {
-    if (key === '\u0003') process.exit();
-  });
-  return stdin;
-};
 setupInput();
