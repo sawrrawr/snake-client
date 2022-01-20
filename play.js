f9cc9c0175433ad594c1d4b2e8fe189cde8d3e4c
Sarah Melanie White <sawrrawr@gmail.com>
2022-01-19 20:33:32 -0330

    added a client.js to handle all the connection information

--- play.js
+++ play.js
@@ -1,19 +1,5 @@
 const net = require('net');
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541',
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const connect = require('./client.js');
 
 console.log('Connecting...');
 connect();
